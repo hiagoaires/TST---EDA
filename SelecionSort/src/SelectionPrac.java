bdd49c3beb4fbcb127dd409d72ba8e30675b1cbb
hiagoaires <142024612+hiagoaires@users.noreply.github.com>
2024-08-06 21:12:24 -0300

    Update SelectionPrac.java

--- SelecionSort/src/SelectionPrac.java
+++ SelecionSort/src/SelectionPrac.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class SelectionPrac {
+class SelectionPrac {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in); // inicia o scanner (input)
@@ -9,7 +9,7 @@ public class SelectionPrac {
 
         String[] elementos = new String[(entrada.split(" ")).length];
         elementos = entrada.split(" ");
-                 // transforma a entrada em um array de strings
+        // transforma a entrada em um array de strings
         int[] arrayInt = new int[elementos.length];
 
         for (int i = 0; i < elementos.length; i++) { //transforma o array de str em um array int
@@ -17,23 +17,26 @@ public class SelectionPrac {
         }
 
 
-        System.out.println(Arrays.toString(selectionSort(arrayInt)));
+        selectionSort(arrayInt);
 
     }
 
-    public static int[] selectionSort(int[] arrayInt) {
+    public static void selectionSort(int[] arrayInt) {
         for (int i = 0; i < arrayInt.length; i++) {
+            boolean houveTroca= false;
             int indexMenor = i;
             for (int j = i + 1; j < arrayInt.length; j++) {
                 if (arrayInt[j] < arrayInt[indexMenor]) {
                     indexMenor = j;
+                    houveTroca= true;
                 }
             }
             int aux = arrayInt[i];
             arrayInt[i] = arrayInt[indexMenor];
             arrayInt[indexMenor] = aux;
 
-        }
-        return arrayInt;
+            if (houveTroca == true){
+            System.out.println(Arrays.toString(arrayInt));
+        }}
     }
 }
